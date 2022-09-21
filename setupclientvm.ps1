5d48c4de1714d5283f20d96601b279462221d275
Thomas Lieser <tliesermn@gmail.com>
2022-09-21 17:20:05 -0500

    install firefox instead of chrome

--- setupclientvm.ps1
+++ setupclientvm.ps1
@@ -1,17 +1,7 @@
-$LocalTempDir = $env:TEMP; 
-$ChromeInstaller = "ChromeInstaller.exe"; 
-(new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; 
-$Process2Monitor =  "ChromeInstaller"; 
-
-Do { 
-	$ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; 
-	If ($ProcessesFound) { 
-		"Still running: $($ProcessesFound -join ', ')" | Write-Host; 
-		Start-Sleep -Seconds 2 
-	} else { 
-		rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose 
-	} 
-} Until (!$ProcessesFound)
+md -Path $env:temp\firefoxinstall -erroraction SilentlyContinue | Out-Null
+$Download = join-path $env:temp\firefoxinstall firefox_installer.exe
+Invoke-WebRequest 'https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US' -OutFile $Download
+Invoke-Expression "$Download /S"
 
 
 
